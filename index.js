4207978f873b8d06de0bd88e3adcf005d3a08e46
kajela <titoyut56@gmail.com>
2023-05-04 03:12:01 -0400

    add the initial files

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+  // const scuberGreetingForFeet = 199;
+  scuberGreetingForFeet = 199;
+  // if(ride === 199){
+  //    someValue = "This one is on me!";
+  // }
+
+  }
 
 function ternaryCheckCity(){
   // Write your code here!
